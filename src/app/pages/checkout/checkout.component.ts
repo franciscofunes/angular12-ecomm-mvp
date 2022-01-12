af86cb1fb3e3d16fcb8a9a3f8c90023bdb246676
Francisco Funes <ffunes@hexacta.com>
2022-01-12 19:08:58 -0300

    feat: create methods in checkout component for adding order details to json server fake api

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { tap } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import {
+  Details,
+  Order,
+} from 'src/app/shared/components/header/interfaces/order.interface';
 import { Store } from 'src/app/shared/components/header/interfaces/stores.interface';
 import { DataService } from 'src/app/shared/components/header/services/data.service';
+import { ShoppingCarService } from 'src/app/shared/components/header/services/shopping-car.service';
+import { Product } from '../products/interface/product.interface';
 
 @Component({
   selector: 'app-checkout',
@@ -16,28 +24,76 @@ export class CheckoutComponent implements OnInit {
     city: '',
   };
 
-  isDelivery = false;
+  isDelivery = true;
 
   stores: Store[] = [];
+  cart: Product[] = [];
 
-  constructor(private dataSvc: DataService) {}
+  constructor(
+    private dataSvc: DataService,
+    private shoppingCartSvc: ShoppingCarService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getStores();
+    this.getDataCart();
+    this.prepareDetails();
   }
 
   onPickupOrDelivery(value: boolean): void {
     this.isDelivery = value;
   }
 
-  onSubmit(): void {
-    console.log('Saved');
+  onSubmit({ value: formData }: NgForm): void {
+    console.log('Saved', formData);
+    const data: Order = {
+      ...formData,
+      date: this.getCurrentDate,
+      pickup: this.isDelivery,
+    };
+    this.dataSvc
+      .saveOrder(data)
+      .pipe(
+        tap((res) => console.log('Order ->', res)),
+        switchMap(({ id: orderId }) => {
+          const details = this.prepareDetails();
+          return this.dataSvc.saveDetailsOrder({ details, orderId });
+        }),
+        tap(() => this.router.navigate(['/thank-you-page']))
+      )
+      .subscribe();
   }
 
   private getStores(): void {
     this.dataSvc
       .getStores()
       .pipe(tap((stores: Store[]) => (this.stores = stores)))
+      .subscribe();
+  }
+
+  private getCurrentDate(): string {
+    return new Date().toLocaleDateString();
+  }
+
+  private prepareDetails(): Details[] {
+    const details: Details[] = [];
+    this.cart.forEach((product: Product) => {
+      const {
+        id: productId,
+        name: productName,
+        qty: quantity,
+        price: price,
+        stock,
+      } = product;
+      details.push({ productId, productName, quantity, price });
+    });
+    return details;
+  }
+
+  private getDataCart(): void {
+    this.shoppingCartSvc.cartAction$
+      .pipe(tap((products: Product[]) => (this.cart = products)))
       .subscribe();
   }
 }
